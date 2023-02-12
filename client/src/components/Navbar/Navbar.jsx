2b8b682388325a5ad7e99740eeeeeeda7f8aeb6f
Faisal <mfaisalhidayatullah@gmail.com>
2023-02-12 19:47:57 +0700

    add header

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+	const [solidNav, setSolidNav] = useState(false);
+	const changeSolidNav = () => {
+		if (window.scrollY >= 40) {
+			setSolidNav(true);
+		} else {
+			setSolidNav(false);
+		}
+	};
+	window.addEventListener("scroll", changeSolidNav);
 	return (
-		<div className="container">
+		<div
+			className={
+				solidNav
+					? "sticky top-0 container bg-transparent z-50 transition-all ease-in duration-300 backdrop-filter backdrop-blur-lg"
+					: "sticky top-0 container bg-transparent z-50 transition-all ease-in duration-300"
+			}
+		>
 			<nav className="flex justify-between items-center ">
 				<NavLink to="/">
 					<img src={Logo} alt="" className="max-w-[60px] md:max-w-[80px]" />
 				</NavLink>
 				<ul>
 					<li>
-						<NavLink to="/" className="mr-6 circleBehind hover:text-[var(--orange)] duration-300">
+						<NavLink
+							to="/"
+							className="mr-6 circleBehind text-[var(--gray-light)] hover:text-[var(--orange)] duration-300"
+						>
 							Home
 						</NavLink>
 						<NavLink
 							to="/write"
-							className="mx-6 circleBehind hover:text-[var(--orange)] duration-300"
+							className="mx-6 circleBehind text-[var(--gray-light)] hover:text-[var(--orange)] duration-300"
 						>
 							Write
 						</NavLink>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
+import "./Home.css";
+
 const Home = () => {
-	return <h1>Home Page</h1>;
+	return (
+		<div className="container text-center text-white text-xl jumbotron">
+			<h2 className="pt-64 text-4xl font-normal mb-2">Welcome In The World Of</h2>
+			<h1 className="text-6xl font-semibold mb-14">
+				Blog <span className="italic text-transparent stroke">Writing</span>
+			</h1>
+			<Link className="btn-orange text-amber-900 py-2">Write A Blog</Link>
+		</div>
+	);
 };
 
 export default Home;
