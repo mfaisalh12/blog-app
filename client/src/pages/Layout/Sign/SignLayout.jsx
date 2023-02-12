58402571d586f1ab5388ed8288cdfefb2f6a0b28
Faisal <mfaisalhidayatullah@gmail.com>
2023-02-12 11:21:11 +0700

    styling responsive with screen 600

--- client/src/pages/Layout/Sign/SignLayout.jsx
+++ client/src/pages/Layout/Sign/SignLayout.jsx
@@ -5,15 +5,15 @@ import logo from "../../../assets/logo.png";
 
 const SignLayout = (props) => {
 	return (
-		<div className="grid grid-cols-2 h-full">
-			<div className="bg-orange h-full">
-				<div className="container mx-auto h-full flex justify-center align-middle">
+		<div className="grid grid-cols-1 md:grid-cols-2 h-full">
+			<div className="bg-orange h-full md:block hidden">
+				<div className="container mx-auto h-full flex justify-center align-middle ">
 					<img src={imageSign} className="self-center lg:max-w-xl" alt="sign" />
 				</div>
 			</div>
 			<div>
 				<div className="container mx-auto h-full lg:py-14">
-					<img src={logo} className="self-center max-w-[40px] md:max-w-[91px]" alt="logo" />
+					<img src={logo} className="self-center max-w-[60px] md:max-w-[91px]" alt="logo" />
 					{props.children}
 				</div>
 			</div>
